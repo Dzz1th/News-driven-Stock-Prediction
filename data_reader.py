59e1814a139d16d7ac121515ebe61707fab2b266
Oleg Yushkevich <halavshik10@gmail.com>
2019-08-13 14:06:35 +0300

    add DataSetFactory and CompanyDataset

--- data_reader.py
+++ data_reader.py
@@ -5,7 +5,8 @@ from torch.utils.data import DataLoader, Dataset
 
 #class DataReader should return text + fin data, if there is text in this day, or only fin data, if no
 
-class FinancialDataReader(DataLoader):
+
+class DataReader(DataLoader):
     def __init__(self, dataset, batch_size, shuffle=True, sampler=None, batch_sampler=None,
     num_workers=0, collate_fn=None):
         self.dataset_obj = dataset #inherit torch Dataset
@@ -16,35 +17,44 @@ class FinancialDataReader(DataLoader):
         self.num_workers = num_workers
         self.collate_fn = collate_fn
 
-    def rolling_window(self, feature, index=None, window_size=3, min_periods=1, win_type=None, func=None):
-        #index - index in iteration process
-        #func must get ndarray and return a single value
-        #min_period == 1 allow to safe first {window_size} values without substitution them by NaN
-
-
-        if index is None:
-            data = self.dataset_obj.dataset
-        else:
-           data = self.dataset.__getitem__(index)   
-        
-        data[feature+'rolling_window'] = data[feature].rolling(window_size, min_periods=min_periods, win_type=win_type).apply(lambda x: func(x)).values
-        return data
-
-    
-
-    
-    
-
     
 
 
-class SequenceDataSet(Dataset):
-    def __init__(self, data, num_day):
-        self.dataset = data
-        self.num_day = num_day
-        
+
+    
+class CompanyDataset(Dataset):
+    def __init(self, company_news_data, company_financial_data, lag):
+        self.company_news_data = company_news_data
+        self.company_financial_data = company_financial_data
+        self.lag = lag
+
     def __getitem__(self, index):
-        return self.dataset.loc[index:index+self.num_day]
-
+        return pd.concat([self.company_news_data[self.company_news_data['date']==index],
+     self.company_financial_data[(self.company_financial_data['date']<(index+self.lag))&(self.company_financial_data['date']>=index)]],
+      axis=1)
+        
     def __len__(self):
-        return self.dataset.shape[0]
+        return self.company_financial_data.shape[0] + self.company_news_data.shape[0]
+
+
+class DatasetFactory():
+    def __init__(self, news_data, financial_data, company_id, company_name, lag):
+        self.news_data = news_data
+        self.financial_data = financial_data
+        self.company_id = company_id
+        self.company_name = company_name
+        self.lag = lag
+    
+    def create_company_dataset(self):
+        company_news_data = self.news_data[is_about(self.news_data['HEADLINE'], self.company_name)] #implement is_about func
+        company_financial_data = self.financial_data[self.financial_data['ws_id'] == self.company_id]
+        return CompanyDataset(company_news_data, company_financial_data, self.lag)
+
+    def set_company_name(self, company_name):
+        self.company_name = company_name
+
+    def set_company_id(self, company_id):
+        self.company_id = company_id
+
+    def set_lag(self, lag):
+        self.lag = lag
